6e6c7094a1004156dfcf90ba6044d6abb05f792d
egely1337 <hyperamabanyiyen@gmail.com>
2024-02-13 21:44:23 +0300

    build problem solved

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -38,6 +38,7 @@ export default function Page(props: {
                 {notifications?.map((val, i) => {
                     return(
                         <Notification
+                            key={i}
                             notification={val}
                         />
                     )
